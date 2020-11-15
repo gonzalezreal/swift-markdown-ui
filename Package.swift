fbbd4b4d8eef30fd8465e399b3a3f93090deaab0
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-11-15 19:55:00 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -5,24 +5,41 @@ import PackageDescription
 
 let package = Package(
     name: "CommonMarkUI",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v11),
+        .tvOS(.v11),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CommonMarkUI",
-            targets: ["CommonMarkUI"]),
+            targets: ["CommonMarkUI"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "cmark",
+            url: "https://github.com/apple/swift-cmark",
+            .revision("9c8096a23f44794bde297452d87c455fc4f76d42")
+        ),
+        .package(
+            name: "SnapshotTesting",
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            from: "1.8.2"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CommonMarkUI",
-            dependencies: []),
+            dependencies: ["cmark"]
+        ),
         .testTarget(
             name: "CommonMarkUITests",
-            dependencies: ["CommonMarkUI"]),
+            dependencies: [
+                "CommonMarkUI",
+                "SnapshotTesting",
+            ],
+            exclude: ["__Snapshots__"]
+        ),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import CommonMarkUITests
-
-var tests = [XCTestCaseEntry]()
-tests += CommonMarkUITests.allTests()
-XCTMain(tests)
