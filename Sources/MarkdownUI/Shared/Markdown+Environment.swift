848a9d303302a0b5c4c0bdecd7df15167459d707
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-10-10 19:21:15 +0200

    Clean slate

--- Sources/MarkdownUI/Shared/Markdown+Environment.swift
+++ Sources/MarkdownUI/Shared/Markdown+Environment.swift
@@ -1,48 +1,29 @@
-#if canImport(SwiftUI) && !os(watchOS)
-  import CombineSchedulers
-  import SwiftUI
+import CombineSchedulers
+import SwiftUI
 
-  extension View {
-    /// Sets the base URL for markdown images in this view and its children.
-    public func markdownBaseURL(_ url: URL?) -> some View {
-      environment(\.markdownBaseURL, url)
-    }
+extension View {
+  /// Sets the markdown style in this view and its children.
+  public func markdownStyle(_ markdownStyle: MarkdownStyle) -> some View {
+    environment(\.markdownStyle, markdownStyle)
+  }
+}
 
-    /// Sets the markdown style in this view and its children.
-    @available(macOS 11.0, *)
-    public func markdownStyle(_ markdownStyle: MarkdownStyle) -> some View {
-      environment(\.markdownStyle, markdownStyle)
-    }
+extension EnvironmentValues {
+  var markdownStyle: MarkdownStyle {
+    get { self[MarkdownStyleKey.self] }
+    set { self[MarkdownStyleKey.self] = newValue }
   }
 
-  extension EnvironmentValues {
-    var markdownBaseURL: URL? {
-      get { self[MarkdownBaseURLKey.self] }
-      set { self[MarkdownBaseURLKey.self] = newValue }
-    }
-
-    @available(macOS 11.0, *)
-    var markdownStyle: MarkdownStyle {
-      get { self[MarkdownStyleKey.self] }
-      set { self[MarkdownStyleKey.self] = newValue }
-    }
-
-    var markdownScheduler: AnySchedulerOf<DispatchQueue> {
-      get { self[MarkdownSchedulerKey.self] }
-      set { self[MarkdownSchedulerKey.self] = newValue }
-    }
+  var markdownScheduler: AnySchedulerOf<DispatchQueue> {
+    get { self[MarkdownSchedulerKey.self] }
+    set { self[MarkdownSchedulerKey.self] = newValue }
   }
+}
 
-  private struct MarkdownBaseURLKey: EnvironmentKey {
-    static let defaultValue: URL? = nil
-  }
+private struct MarkdownStyleKey: EnvironmentKey {
+  static let defaultValue: MarkdownStyle = .system
+}
 
-  @available(macOS 11.0, *)
-  private struct MarkdownStyleKey: EnvironmentKey {
-    static let defaultValue: MarkdownStyle = DefaultMarkdownStyle(font: .system(.body))
-  }
-
-  private struct MarkdownSchedulerKey: EnvironmentKey {
-    static let defaultValue: AnySchedulerOf<DispatchQueue> = .main
-  }
-#endif
+private struct MarkdownSchedulerKey: EnvironmentKey {
+  static let defaultValue: AnySchedulerOf<DispatchQueue> = .main
+}
