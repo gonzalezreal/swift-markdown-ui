b727433fdb49f0bf3a763f95888a6a6461d6aad7
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-11-18 11:54:31 +0100

    Render headings

--- Sources/MarkdownUI/Style/Measurements.swift
+++ Sources/MarkdownUI/Style/Measurements.swift
@@ -7,13 +7,17 @@ extension MarkdownStyle {
     public var tailIndentStep: CGFloat
     public var paragraphSpacing: CGFloat
     public var listMarkerSpacing: CGFloat
+    public var headingScale: (CGFloat, CGFloat, CGFloat, CGFloat, CGFloat, CGFloat)
+    public var headingSpacing: CGFloat
 
     public static let `default` = Measurements(
       codeFontScale: 0.94,
       headIndentStep: 1.97,
       tailIndentStep: -1,
       paragraphSpacing: 1,
-      listMarkerSpacing: 0.47
+      listMarkerSpacing: 0.47,
+      headingScale: (2, 1.5, 1.17, 1, 0.83, 0.67),
+      headingSpacing: 0.67
     )
 
     public init(
@@ -21,13 +25,17 @@ extension MarkdownStyle {
       headIndentStep: CGFloat,
       tailIndentStep: CGFloat,
       paragraphSpacing: CGFloat,
-      listMarkerSpacing: CGFloat
+      listMarkerSpacing: CGFloat,
+      headingScale: (CGFloat, CGFloat, CGFloat, CGFloat, CGFloat, CGFloat),
+      headingSpacing: CGFloat
     ) {
       self.codeFontScale = codeFontScale
       self.headIndentStep = headIndentStep
       self.tailIndentStep = tailIndentStep
       self.paragraphSpacing = paragraphSpacing
       self.listMarkerSpacing = listMarkerSpacing
+      self.headingScale = headingScale
+      self.headingSpacing = headingSpacing
     }
   }
 }
