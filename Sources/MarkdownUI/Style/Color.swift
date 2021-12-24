38d61849b5158d4932e23a362416e6cbcd42f948
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-12-24 17:25:23 +0100

    Refactor Color and make it hashable

--- Sources/MarkdownUI/Style/Color.swift
+++ Sources/MarkdownUI/Style/Color.swift
@@ -1,119 +1,110 @@
 import SwiftUI
 
 extension MarkdownStyle {
-  public struct Color {
-    var resolve: () -> PlatformColor?
+  public struct Color: Hashable {
+    var platformColor: PlatformColor?
   }
 }
 
 extension MarkdownStyle.Color {
   /// A context-dependent red color suitable for use in UI elements.
-  public static let red = Self { .systemRed }
+  public static let red = Self(platformColor: .systemRed)
 
   /// A context-dependent orange color suitable for use in UI elements.
-  public static let orange = Self { .systemOrange }
+  public static let orange = Self(platformColor: .systemOrange)
 
   /// A context-dependent yellow color suitable for use in UI elements.
-  public static let yellow = Self { .systemYellow }
+  public static let yellow = Self(platformColor: .systemYellow)
 
   /// A context-dependent green color suitable for use in UI elements.
-  public static let green = Self { .systemGreen }
+  public static let green = Self(platformColor: .systemGreen)
 
   /// A context-dependent teal color suitable for use in UI elements.
-  public static let teal = Self { .systemTeal }
+  public static let teal = Self(platformColor: .systemTeal)
 
   /// A context-dependent blue color suitable for use in UI elements.
-  public static let blue = Self { .systemBlue }
+  public static let blue = Self(platformColor: .systemBlue)
 
   /// A context-dependent indigo color suitable for use in UI elements.
-  public static let indigo = Self { .systemIndigo }
+  public static let indigo = Self(platformColor: .systemIndigo)
 
   /// A context-dependent purple color suitable for use in UI elements.
-  public static let purple = Self { .systemPurple }
+  public static let purple = Self(platformColor: .systemPurple)
 
   /// A context-dependent pink color suitable for use in UI elements.
-  public static let pink = Self { .systemPink }
+  public static let pink = Self(platformColor: .systemPink)
 
   /// A white color suitable for use in UI elements.
-  public static let white = Self { .white }
+  public static let white = Self(platformColor: .white)
 
   /// A context-dependent gray color suitable for use in UI elements.
-  public static let gray = Self { .systemGray }
+  public static let gray = Self(platformColor: .systemGray)
 
   /// A black color suitable for use in UI elements.
-  public static let black = Self { .black }
+  public static let black = Self(platformColor: .black)
 
   /// A clear color suitable for use in UI elements.
-  public static let clear = Self { .clear }
+  public static let clear = Self(platformColor: .clear)
 
-  /// The color to use for primary content.
-  public static let primary = Self {
-    #if os(macOS)
-      return .labelColor
-    #elseif os(iOS) || os(tvOS)
-      return .label
-    #endif
-  }
+  #if os(macOS)
+    /// The color to use for primary content.
+    public static let primary = Self(platformColor: .labelColor)
+  #elseif os(iOS) || os(tvOS)
+    /// The color to use for primary content.
+    public static let primary = Self(platformColor: .label)
+  #endif
 
-  /// The color to use for secondary content.
-  public static let secondary = Self {
-    #if os(macOS)
-      return .secondaryLabelColor
-    #elseif os(iOS) || os(tvOS)
-      return .secondaryLabel
-    #endif
-  }
+  #if os(macOS)
+    /// The color to use for secondary content.
+    public static let secondary = Self(platformColor: .secondaryLabelColor)
+  #elseif os(iOS) || os(tvOS)
+    /// The color to use for secondary content.
+    public static let secondary = Self(platformColor: .secondaryLabel)
+  #endif
 
-  /// The color to use for separators between different sections of content.
-  public static let separator = Self {
-    #if os(macOS)
-      return .separatorColor
-    #elseif os(iOS) || os(tvOS)
-      return .separator
-    #endif
-  }
+  #if os(macOS)
+    /// The color to use for separators between different sections of content.
+    public static let separator = Self(platformColor: .separatorColor)
+  #elseif os(iOS) || os(tvOS)
+    /// The color to use for separators between different sections of content.
+    public static let separator = Self(platformColor: .separator)
+  #endif
 
   /// Creates a color from a Core Graphics color.
   public init(cgColor: CGColor) {
-    self.init {
-      .init(cgColor: cgColor)
-    }
+    self.init(platformColor: .init(cgColor: cgColor))
   }
 
   /// Creates a constant color from red, green, and blue component values.
   public init(red: CGFloat, green: CGFloat, blue: CGFloat, opacity: CGFloat = 1) {
-    self.init {
-      .init(red: red, green: green, blue: blue, alpha: opacity)
-    }
+    self.init(platformColor: .init(red: red, green: green, blue: blue, alpha: opacity))
   }
 
   /// Creates a constant grayscale color.
   public init(white: CGFloat, opacity: CGFloat = 1) {
-    self.init {
-      .init(white: white, alpha: opacity)
-    }
+    self.init(platformColor: .init(white: white, alpha: opacity))
   }
 
   /// Creates a constant color from hue, saturation, and brightness values.
   public init(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, opacity: CGFloat = 1) {
-    self.init {
-      .init(hue: hue, saturation: saturation, brightness: brightness, alpha: opacity)
-    }
+    self.init(
+      platformColor: .init(
+        hue: hue, saturation: saturation, brightness: brightness, alpha: opacity
+      )
+    )
   }
 
   /// Creates a color from a color set that you indicate by name.
   public init(_ name: String, bundle: Bundle? = nil) {
-    self.init {
-      #if os(macOS)
-        return .init(named: name, bundle: bundle)
-      #elseif os(iOS) || os(tvOS)
-        return .init(named: name, in: bundle, compatibleWith: nil)
-      #endif
-    }
+    #if os(macOS)
+      self.init(platformColor: .init(named: name, bundle: bundle))
+    #elseif os(iOS) || os(tvOS)
+      self.init(platformColor: .init(named: name, in: bundle, compatibleWith: nil))
+    #endif
   }
 
   public func opacity(_ opacity: CGFloat) -> MarkdownStyle.Color {
-    modifier(.opacity(opacity))
+    .init(platformColor: self.platformColor?.withAlphaComponent(opacity))
   }
 }
 
@@ -124,7 +115,7 @@ extension MarkdownStyle.Color {
   extension MarkdownStyle.Color {
     /// Creates a color from an AppKit color.
     public init(nsColor: NSColor) {
-      self.init { nsColor }
+      self.init(platformColor: nsColor)
     }
   }
 #endif
@@ -136,38 +127,10 @@ extension MarkdownStyle.Color {
   extension MarkdownStyle.Color {
     /// Creates a color from an UIKit color.
     public init(uiColor: UIColor) {
-      self.init { uiColor }
+      self.init(platformColor: uiColor)
     }
   }
 #endif
-
-extension MarkdownStyle.Color {
-  fileprivate func modifier(_ modifier: MarkdownStyle.ColorModifier) -> MarkdownStyle.Color {
-    MarkdownStyle.Color {
-      guard var platformColor = self.resolve() else {
-        return nil
-      }
-      modifier.modify(&platformColor)
-      return platformColor
-    }
-  }
-}
-
-// MARK: - ColorModifier
-
-extension MarkdownStyle {
-  fileprivate struct ColorModifier {
-    var modify: (inout PlatformColor) -> Void
-  }
-}
-
-extension MarkdownStyle.ColorModifier {
-  static func opacity(_ opacity: CGFloat) -> MarkdownStyle.ColorModifier {
-    .init { platformColor in
-      platformColor = platformColor.withAlphaComponent(opacity)
-    }
-  }
-}
 
 // MARK: - PlatformColor
 
