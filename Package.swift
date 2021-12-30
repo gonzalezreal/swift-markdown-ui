bf2f4c7dafa3cf4306f87b42e4df02d5b2a5af7b
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-12-30 12:41:06 +0100

    AttributedText++

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     .package(
       name: "AttributedText",
       url: "https://github.com/gonzalezreal/AttributedText",
-      from: "1.0.0-beta.2"
+      from: "1.0.0"
     ),
     .package(
       name: "NetworkImage",
