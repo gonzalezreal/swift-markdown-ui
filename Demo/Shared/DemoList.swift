434ac59b475ed2d0e3987dccaf148b0085f24e01
Guille Gonzalez <guillermo.gonzalez@me.com>
2022-01-03 08:36:48 +0100

    Add Images, Links, Dingus and Read Me demos

---
+++ Demo/Shared/DemoList.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct DemoList: View {
+  var body: some View {
+    NavigationView {
+      #if os(macOS)
+        self.list
+          .listStyle(.sidebar)
+        Text("Select a group")
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+      #else
+        self.list
+          .navigationTitle("MarkdownUI")
+        Text("Select a group")
+      #endif
+    }
+  }
+
+  private var list: some View {
+    List {
+      DemoRow(title: "Images", systemImage: "photo") {
+        ImagesGroup()
+      }
+      #if !os(tvOS)
+        DemoRow(title: "Links", systemImage: "link") {
+          LinksGroup()
+        }
+        DemoRow(title: "Dingus", systemImage: "character.cursor.ibeam") {
+          DingusGroup()
+        }
+        DemoRow(title: "Read Me", systemImage: "doc.text") {
+          ReadMeGroup()
+        }
+      #endif
+    }
+  }
+}
+
+struct DemoList_Previews: PreviewProvider {
+  static var previews: some View {
+    DemoList()
+  }
+}
--- Demo/Shared/MarkdownUIDemoApp.swift
+++ Demo/Shared/MarkdownUIDemoApp.swift
@@ -4,9 +4,19 @@ import SwiftUI
 struct MarkdownUIDemoApp: App {
   var body: some Scene {
     WindowGroup {
-      NavigationView {
-        ExampleList(examples: Example.all)
-      }
+      #if os(macOS)
+        DemoList()
+          .frame(
+            minWidth: 100,
+            idealWidth: 300,
+            maxWidth: .infinity,
+            minHeight: 100,
+            idealHeight: 200,
+            maxHeight: .infinity
+          )
+      #else
+        DemoList()
+      #endif
     }
   }
 }
