e04c3adff6294d415a18b1d8d9051e053f63ba78
Guille Gonzalez <guillermo.gonzalez@me.com>
2022-01-07 19:07:06 +0100

    Move document rendering to an extension

--- Sources/MarkdownUI/Rendering/Document+NSAttributedString.swift
+++ Sources/MarkdownUI/Rendering/Document+NSAttributedString.swift
@@ -1,11 +1,12 @@
+import Combine
 import CommonMark
 import SwiftUI
 
 extension Document {
-  public func renderAttributedString(
+  func renderAttributedString(
     baseURL: URL?,
-    baseWritingDirection: NSWritingDirection = .natural,
-    alignment: NSTextAlignment = .natural,
+    baseWritingDirection: NSWritingDirection,
+    alignment: NSTextAlignment,
     style: MarkdownStyle
   ) -> NSAttributedString {
     AttributedStringRenderer(
@@ -14,5 +15,36 @@ extension Document {
       alignment: alignment,
       style: style
     ).renderDocument(self)
+  }
+
+  func renderAttributedString(
+    baseURL: URL?,
+    baseWritingDirection: NSWritingDirection,
+    alignment: NSTextAlignment,
+    style: MarkdownStyle,
+    imageHandlers: [String: MarkdownImageHandler]
+  ) -> AnyPublisher<NSAttributedString, Never> {
+    Deferred {
+      Just(
+        self.renderAttributedString(
+          baseURL: baseURL,
+          baseWritingDirection: baseWritingDirection,
+          alignment: alignment,
+          style: style
+        )
+      )
+    }
+    .flatMap { attributedString -> AnyPublisher<NSAttributedString, Never> in
+      guard attributedString.hasMarkdownImages else {
+        return Just(attributedString).eraseToAnyPublisher()
+      }
+      return NSAttributedString.loadingMarkdownImages(
+        from: attributedString,
+        using: imageHandlers
+      )
+      .prepend(attributedString)
+      .eraseToAnyPublisher()
+    }
+    .eraseToAnyPublisher()
   }
 }
