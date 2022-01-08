cff5536f5a2b1e24cb732e9f13b518de262fa55e
Guille Gonzalez <guillermo.gonzalez@me.com>
2022-01-08 16:08:05 +0100

    Document MarkdownStyle and its nested types

--- Sources/MarkdownUI/Style/HeadingScales.swift
+++ Sources/MarkdownUI/Style/HeadingScales.swift
@@ -1,9 +1,17 @@
 import SwiftUI
 
 extension MarkdownStyle {
+  /// The scale that the `Markdown` view applies to each heading level.
+  ///
+  /// You use a `HeadingScales` instance to configure the heading sizes in
+  /// a `Markdown` view. When the `Markdown` view encounters a heading,
+  /// it computes the font size by multiplying the current font size by the scale
+  /// specified for that heading level.
   public struct HeadingScales: Hashable {
     private var values: [CGFloat]
 
+    /// Creates a `HeadingScales` instance with the provided scales for each
+    /// heading level.
     public init(
       h1: CGFloat,
       h2: CGFloat,
@@ -19,6 +27,7 @@ extension MarkdownStyle {
       values[index]
     }
 
+    /// The default heading scales.
     public static let `default` = Self(h1: 2, h2: 1.5, h3: 1.17, h4: 1, h5: 0.83, h6: 0.67)
   }
 }
