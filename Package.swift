4c32d4c2163564bdeeeb49173aaf0c92e8b33438
Guille Gonzalez <guillermo.gonzalez@glovoapp.com>
2022-08-21 11:12:39 +0200

    Blank slate (#120)

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
   name: "MarkdownUI",
   platforms: [
-    .macOS(.v11),
-    .iOS(.v14),
-    .tvOS(.v14),
+    .macOS(.v12),
+    .iOS(.v15),
+    .tvOS(.v15),
   ],
   products: [
     .library(
@@ -16,43 +16,18 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(
-      name: "SwiftCommonMark",
-      url: "https://github.com/gonzalezreal/SwiftCommonMark",
-      from: "1.0.0"
-    ),
-    .package(
-      name: "AttributedText",
-      url: "https://github.com/gonzalezreal/AttributedText",
-      from: "1.0.0"
-    ),
-    .package(
-      name: "NetworkImage",
-      url: "https://github.com/gonzalezreal/NetworkImage",
-      from: "4.0.0"
-    ),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.5.3"),
-    .package(
-      name: "SnapshotTesting",
-      url: "https://github.com/pointfreeco/swift-snapshot-testing",
-      from: "1.9.0"
-    ),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.9.0")
   ],
   targets: [
     .target(
       name: "MarkdownUI",
-      dependencies: [
-        .product(name: "CommonMark", package: "SwiftCommonMark"),
-        "AttributedText",
-        "NetworkImage",
-        .product(name: "CombineSchedulers", package: "combine-schedulers"),
-      ]
+      dependencies: []
     ),
     .testTarget(
       name: "MarkdownUITests",
       dependencies: [
         "MarkdownUI",
-        "SnapshotTesting",
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
       ],
       exclude: ["__Snapshots__"]
     ),
