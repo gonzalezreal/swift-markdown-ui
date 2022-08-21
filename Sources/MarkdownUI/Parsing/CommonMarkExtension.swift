f9879a077ca8ba95980ceb064e68c142bba712a8
gonzalezreal <guillermo.gonzalez@glovoapp.com>
2022-08-21 13:06:52 +0200

    Add cmark-gfm-0.29.0.gfm.4

---
+++ Sources/MarkdownUI/Parsing/CommonMarkExtension.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+internal struct CommonMarkExtension: Hashable, RawRepresentable {
+  let rawValue: String
+
+  init(rawValue: String) {
+    self.rawValue = rawValue
+  }
+}
+
+extension CommonMarkExtension {
+  static let autolink = Self(rawValue: "autolink")
+  static let strikethrough = Self(rawValue: "strikethrough")
+  static let table = Self(rawValue: "table")
+  static let tagfilter = Self(rawValue: "tagfilter")
+  static let tasklist = Self(rawValue: "tasklist")
+}
+
+extension Set where Element == CommonMarkExtension {
+  static let all: Self = [.autolink, .strikethrough, .table, .tagfilter, .tasklist]
+}
