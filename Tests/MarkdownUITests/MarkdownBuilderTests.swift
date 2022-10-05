c7dce7ed714422326da7955efbaf678f51febe83
gonzalezreal <guillermo.gonzalez@glovoapp.com>
2022-10-05 21:19:55 +0200

    Run swift-format

--- Tests/MarkdownUITests/MarkdownBuilderTests.swift
+++ Tests/MarkdownUITests/MarkdownBuilderTests.swift
@@ -5,97 +5,97 @@
 
   import MarkdownUI
 
-final class MarkdownBuilderTests: XCTestCase {
-  private let layout = SwiftUISnapshotLayout.device(config: .iPhone8)
+  final class MarkdownBuilderTests: XCTestCase {
+    private let layout = SwiftUISnapshotLayout.device(config: .iPhone8)
 
-  func testInlineContentBuilder() {
-    let view = Markdown {
-      Paragraph {}
-      Paragraph {
-        "Hello world!"
-      }
-      Paragraph {
-        "Hello "
-        Strong {
-          "world"
+    func testInlineContentBuilder() {
+      let view = Markdown {
+        Paragraph {}
+        Paragraph {
+          "Hello world!"
         }
-        "!"
-      }
-      Paragraph {
-        for i in 1..<5 {
-          if i == 1 {
-            "World "
-          } else {
-            "word "
+        Paragraph {
+          "Hello "
+          Strong {
+            "world"
           }
-          Emphasis {
-            "\(i)"
-          }
-          if i < 4 {
-            ", "
-          }
+          "!"
         }
-        "."
+        Paragraph {
+          for i in 1..<5 {
+            if i == 1 {
+              "World "
+            } else {
+              "word "
+            }
+            Emphasis {
+              "\(i)"
+            }
+            if i < 4 {
+              ", "
+            }
+          }
+          "."
+        }
       }
+
+      assertSnapshot(matching: view, as: .image(layout: layout))
     }
 
-    assertSnapshot(matching: view, as: .image(layout: layout))
-  }
+    func testBlockContentBuilder() {
+      let view = Markdown {
+        Paragraph {
+          "First paragraph!"
+        }
+        for i in 2..<5 {
+          "Paragraph _\(i)_."
+        }
+        "Last paragraph."
+      }
 
-  func testBlockContentBuilder() {
-    let view = Markdown {
-      Paragraph {
-        "First paragraph!"
-      }
-      for i in 2..<5 {
-        "Paragraph _\(i)_."
-      }
-      "Last paragraph."
+      assertSnapshot(matching: view, as: .image(layout: layout))
     }
 
-    assertSnapshot(matching: view, as: .image(layout: layout))
-  }
+    func testListContentBuilder() {
+      let data = [
+        (name: "Hats", isDone: true),
+        (name: "Caps", isDone: false),
+        (name: "Bonnets", isDone: false),
+      ]
 
-  func testListContentBuilder() {
-    let data = [
-      (name: "Hats", isDone: true),
-      (name: "Caps", isDone: false),
-      (name: "Bonnets", isDone: false)
-    ]
-
-    let view = Markdown {
-      NumberedList(data: data) { item in
-        ListItem {
-          "**Headgear**: \(item.name)"
-        }
-      }
-      BulletedList(data: data) { item in
-        ListItem {
-          "**Headgear**: \(item.name)"
-        }
-      }
-      TaskList(data: data) { item in
-        ListItem(checkbox: item.isDone ? .checked : .unchecked) {
-          "**Headgear**: \(item.name)"
-        }
-      }
-      NumberedList {
-        ListItem {
-          "This is the first item."
-        }
-        for item in data {
+      let view = Markdown {
+        NumberedList(data: data) { item in
           ListItem {
             "**Headgear**: \(item.name)"
           }
         }
-        ListItem {
-          "This is the last item."
+        BulletedList(data: data) { item in
+          ListItem {
+            "**Headgear**: \(item.name)"
+          }
+        }
+        TaskList(data: data) { item in
+          ListItem(checkbox: item.isDone ? .checked : .unchecked) {
+            "**Headgear**: \(item.name)"
+          }
+        }
+        NumberedList {
+          ListItem {
+            "This is the first item."
+          }
+          for item in data {
+            ListItem {
+              "**Headgear**: \(item.name)"
+            }
+          }
+          ListItem {
+            "This is the last item."
+          }
         }
       }
-    }
 
-    assertSnapshot(matching: view, as: .image(layout: layout))
+      assertSnapshot(matching: view, as: .image(layout: layout))
+    }
   }
-}
 
 #endif
