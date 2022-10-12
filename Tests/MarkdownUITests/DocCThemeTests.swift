9165eceb8ddc51746ae63f1a7223649346cb3406
gonzalezreal <guillermo.gonzalez@glovoapp.com>
2022-10-12 11:56:43 +0200

    Use BlockContentBuilder

--- Tests/MarkdownUITests/DocCThemeTests.swift
+++ Tests/MarkdownUITests/DocCThemeTests.swift
@@ -6,20 +6,20 @@
   import MarkdownUI
 
   final class DocCThemeTests: XCTestCase {
-    private struct TestView: View {
-      var markdown: String
+    private struct TestView<Content: BlockContent>: View {
+      private let content: () -> Content
 
-      init(_ markdown: String) {
-        self.markdown = markdown
+      init(@BlockContentBuilder content: @escaping () -> Content) {
+        self.content = content
       }
 
       var body: some View {
         VStack {
-          Markdown(markdown)
+          Markdown(content: content)
             .padding()
             .background(Color(.systemBackground))
             .colorScheme(.light)
-          Markdown(markdown)
+          Markdown(content: content)
             .padding()
             .background(Color(.systemBackground))
             .colorScheme(.dark)
@@ -32,7 +32,7 @@
     private let layout = SwiftUISnapshotLayout.device(config: .iPhone8)
 
     func testImage() {
-      let view = TestView(
+      let view = TestView {
         #"""
         100x150 image:
 
@@ -40,21 +40,21 @@
 
         ― Photo by André Spieker
         """#
-      )
+      }
       .markdownImageLoader(.asset(in: .module), forURLScheme: "asset")
 
       assertSnapshot(matching: view, as: .image(layout: layout))
     }
 
     func testBlockQuote() {
-      let view = TestView(
+      let view = TestView {
         #"""
         > Outside of a dog, a book is man's best friend. Inside of a
         > dog it's too dark to read.
-
+        
         ― Groucho Marx
         """#
-      )
+      }
 
       assertSnapshot(matching: view, as: .image(layout: layout))
     }
