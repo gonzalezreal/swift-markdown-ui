421afbb657681b213571abc4babbdc0610587614
gonzalezreal <gonzalezreal@icloud.com>
2022-10-31 19:42:04 +0100

    Make spacing and spacingBefore preferences optional

--- Sources/MarkdownUI/Views/Blocks/SpacingBeforePreference.swift
+++ Sources/MarkdownUI/Views/Blocks/SpacingBeforePreference.swift
@@ -4,13 +4,15 @@ private struct SpacingBeforePreference: PreferenceKey {
   static let defaultValue: CGFloat = 0
 
   static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-    value = max(value, nextValue())
+    value = nextValue()
   }
 }
 
 extension View {
-  public func spacingBeforePreference(_ value: CGFloat) -> some View {
-    self.preference(key: SpacingBeforePreference.self, value: value)
+  public func spacingBeforePreference(_ value: CGFloat?) -> some View {
+    self.transformPreference(SpacingBeforePreference.self) { spacingBefore in
+      spacingBefore = value ?? spacingBefore
+    }
   }
 
   func onSpacingBeforePreferenceChange(perform action: @escaping (CGFloat) -> Void) -> some View {
--- Sources/MarkdownUI/Views/Blocks/SpacingPreference.swift
+++ Sources/MarkdownUI/Views/Blocks/SpacingPreference.swift
@@ -4,13 +4,15 @@ private struct SpacingPreference: PreferenceKey {
   static let defaultValue: CGFloat = Font.TextStyle.body.pointSize
 
   static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-    value = max(value, nextValue())
+    value = nextValue()
   }
 }
 
 extension View {
-  public func spacingPreference(_ value: CGFloat) -> some View {
-    self.preference(key: SpacingPreference.self, value: value)
+  public func spacingPreference(_ value: CGFloat?) -> some View {
+    self.transformPreference(SpacingPreference.self) { spacing in
+      spacing = value ?? spacing
+    }
   }
 
   func onSpacingPreferenceChange(perform action: @escaping (CGFloat) -> Void) -> some View {
