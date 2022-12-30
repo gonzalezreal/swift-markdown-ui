141f664fb5183e41e85070b22232670924f3cd16
Guille Gonzalez <gonzalezreal@icloud.com>
2022-12-30 17:46:31 +0100

    Disable font properties test in tvOS

--- Tests/MarkdownUITests/FontPropertiesTests.swift
+++ Tests/MarkdownUITests/FontPropertiesTests.swift
@@ -1,89 +1,91 @@
-import SwiftUI
-import XCTest
+#if !os(tvOS)
+  import SwiftUI
+  import XCTest
 
-@testable import MarkdownUI
+  @testable import MarkdownUI
 
-final class FontPropertiesTests: XCTestCase {
-  func testFontWithProperties() {
-    // given
-    var fontProperties = FontProperties()
+  final class FontPropertiesTests: XCTestCase {
+    func testFontWithProperties() {
+      // given
+      var fontProperties = FontProperties()
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(family: .custom("Menlo"))
+      // when
+      fontProperties = FontProperties(family: .custom("Menlo"))
 
-    // then
-    XCTAssertEqual(
-      Font.custom("Menlo", size: FontProperties.defaultSize),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.custom("Menlo", size: FontProperties.defaultSize),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(familyVariant: .monospaced)
+      // when
+      fontProperties = FontProperties(familyVariant: .monospaced)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default).monospaced(),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default).monospaced(),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(capsVariant: .lowercaseSmallCaps)
+      // when
+      fontProperties = FontProperties(capsVariant: .lowercaseSmallCaps)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default).lowercaseSmallCaps(),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default).lowercaseSmallCaps(),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(digitVariant: .monospaced)
+      // when
+      fontProperties = FontProperties(digitVariant: .monospaced)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default).monospacedDigit(),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default).monospacedDigit(),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(style: .italic)
+      // when
+      fontProperties = FontProperties(style: .italic)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default).italic(),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default).italic(),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(weight: .heavy)
+      // when
+      fontProperties = FontProperties(weight: .heavy)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: FontProperties.defaultSize, design: .default).weight(.heavy),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: FontProperties.defaultSize, design: .default).weight(.heavy),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(size: 42)
+      // when
+      fontProperties = FontProperties(size: 42)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: 42, design: .default),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: 42, design: .default),
+        Font.withProperties(fontProperties)
+      )
 
-    // when
-    fontProperties = FontProperties(scale: 1.5)
+      // when
+      fontProperties = FontProperties(scale: 1.5)
 
-    // then
-    XCTAssertEqual(
-      Font.system(size: round(FontProperties.defaultSize * 1.5), design: .default),
-      Font.withProperties(fontProperties)
-    )
+      // then
+      XCTAssertEqual(
+        Font.system(size: round(FontProperties.defaultSize * 1.5), design: .default),
+        Font.withProperties(fontProperties)
+      )
+    }
   }
-}
+#endif
