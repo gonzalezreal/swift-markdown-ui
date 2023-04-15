3830c4c77bf93b4c7c85851622237f7552e73c9d
Guille Gonzalez <gonzalezreal@icloud.com>
2023-04-15 10:46:33 +0200

    Render HTML line breaks (#221)

--- Sources/MarkdownUI/Renderer/TextInlineRenderer.swift
+++ Sources/MarkdownUI/Renderer/TextInlineRenderer.swift
@@ -25,6 +25,7 @@ private struct TextInlineRenderer {
   private let textStyles: InlineTextStyles
   private let images: [String: Image]
   private let attributes: AttributeContainer
+  private var shouldSkipNextWhitespace = false
 
   init(
     baseURL: URL?,
@@ -46,20 +47,65 @@ private struct TextInlineRenderer {
 
   private mutating func render(_ inline: InlineNode) {
     switch inline {
+    case .text(let content):
+      self.renderText(content)
+    case .softBreak:
+      self.renderSoftBreak()
+    case .html(let content):
+      self.renderHTML(content)
     case .image(let source, _):
-      if let image = self.images[source] {
-        self.result = self.result + Text(image)
-      }
+      self.renderImage(source)
     default:
-      self.result =
-        self.result
-        + Text(
-          inline.renderAttributedString(
-            baseURL: self.baseURL,
-            textStyles: self.textStyles,
-            attributes: self.attributes
-          )
-        )
+      self.defaultRender(inline)
     }
+  }
+
+  private mutating func renderText(_ text: String) {
+    var text = text
+
+    if self.shouldSkipNextWhitespace {
+      self.shouldSkipNextWhitespace = false
+      text = text.replacingOccurrences(of: "^\\s+", with: "", options: .regularExpression)
+    }
+
+    self.defaultRender(.text(text))
+  }
+
+  private mutating func renderSoftBreak() {
+    if self.shouldSkipNextWhitespace {
+      self.shouldSkipNextWhitespace = false
+    } else {
+      self.defaultRender(.softBreak)
+    }
+  }
+
+  private mutating func renderHTML(_ html: String) {
+    let tag = HTMLTag(html)
+
+    switch tag?.name.lowercased() {
+    case "br":
+      self.defaultRender(.lineBreak)
+      self.shouldSkipNextWhitespace = true
+    default:
+      self.defaultRender(.html(html))
+    }
+  }
+
+  private mutating func renderImage(_ source: String) {
+    if let image = self.images[source] {
+      self.result = self.result + Text(image)
+    }
+  }
+
+  private mutating func defaultRender(_ inline: InlineNode) {
+    self.result =
+      self.result
+      + Text(
+        inline.renderAttributedString(
+          baseURL: self.baseURL,
+          textStyles: self.textStyles,
+          attributes: self.attributes
+        )
+      )
   }
 }
