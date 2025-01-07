476431d02eb3bb8793adb5567e28e637e87b562d
Aterzhang13 <34270081+CoderAT13@users.noreply.github.com>
2025-01-07 10:04:09 +0800

    Feature/yytext (#1)
    * feat: YYText引入
    
    * feat: YYText引入
    
    * feat: YYText引入
    
    ---------
    
    Co-authored-by: aterzhang <aterzhang@tencent.com>

--- Sources/MarkdownUI/Renderer/TextInlineRenderer.swift
+++ Sources/MarkdownUI/Renderer/TextInlineRenderer.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import YYText
 
 extension Sequence where Element == InlineNode {
   func renderText(
@@ -63,6 +64,13 @@ private struct TextInlineRenderer {
       self.renderSoftBreak()
     case .html(let content):
       self.renderHTML(content)
+    case .link(let destination, let children):
+        if destination == "@ref" {
+            let text = children.renderPlainText()
+            self.renderIndexLink(text)
+        } else {
+            self.defaultRender(inline)
+        }
     case .image(let source, _):
       self.renderImage(source)
     default:
@@ -108,6 +116,40 @@ private struct TextInlineRenderer {
   private mutating func renderImage(_ source: String) {
 
   }
+    
+    private mutating func renderIndexLink(_ text: String) {
+        guard text.count > 0 else { return }
+        
+        let component = text.components(separatedBy: ",")
+        for index in component {
+            self.defaultRender(.text(" "))
+            addIndexAttributedString(index)
+        }
+    }
+    
+    private mutating func addIndexAttributedString(_ index: String) {
+        let width = 18
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: width))
+        label.backgroundColor = UIColor(hex: "7ABC98").withAlphaComponent(0.4)
+        label.textColor = UIColor(hex: "206D48")
+        label.text = index
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 11, weight: .light)
+        label.layer.cornerRadius = 6
+        label.clipsToBounds = true
+        label.isUserInteractionEnabled = true
+        
+        let tapGr = UITapGestureRecognizer(target: MDEventManager.shared,
+                                           action: #selector(MDEventManager.shared.onIndexClicked(_:)))
+        label.addGestureRecognizer(tapGr)
+        
+        let font = UIFont.systemFont(ofSize: 16, weight: .light)
+        let switchString = NSMutableAttributedString.yy_attachmentString(withContent: label,
+                                                                         contentMode: .center,
+                                                                         attachmentSize: label.frame.size,
+                                                                         alignTo: font, alignment: .center)
+        self.result.appendNSAttributeString(switchString)
+    }
 
   private mutating func defaultRender(_ inline: InlineNode) {
       result.append(
@@ -118,21 +160,18 @@ private struct TextInlineRenderer {
           attributes: self.attributes,
           textReplacer: self.textReplacer
         )
-        )
+    )
   }
 }
 
 // 添加可选择文本的TextView
 struct SelectableText: UIViewRepresentable {
     @Environment(\.openURL) private var openURL
-    var attributedString: AttributedString
     var text: NSAttributedString
     var lineSpacing: CGFloat = 12
     
     init(_ attributedString: AttributedString) {
-        self.attributedString = AttributedString() + attributedString
-        self.attributedString.foregroundColor = .systemBlue
-        text = NSAttributedString("")
+        text = NSAttributedString()
     }
     
     func makeCoordinator() -> Coordinator {
@@ -140,36 +179,46 @@ struct SelectableText: UIViewRepresentable {
     }
     
     mutating func append(_ attributedString: AttributedString) {
-        self.attributedString += attributedString
-        self.text = transfromAttributedText()
+        let attrt = NSMutableAttributedString(attributedString: self.text)
+        let newAttr = transfromAttributedText(attributedString)
+        attrt.append(newAttr)
+        
+        self.text = attrt
+    }
+    
+    mutating func appendNSAttributeString(_ nsAttributedString: NSAttributedString) {
+        let attrt = NSMutableAttributedString(attributedString: self.text)
+        attrt.append(nsAttributedString)
+        
+        self.text = attrt
     }
     
     mutating func textViewLineSpacing(_ spacing: CGFloat) {
         self.lineSpacing = spacing
     }
     
-    func makeUIView(context: Context) -> UITextView {
-        let textView = UITextView()
+    func makeUIView(context: Context) -> YYLabel {
+        let textView = YYLabel()
         textView.backgroundColor = .clear
-        textView.isEditable = false
-        textView.isSelectable = true
-        textView.isScrollEnabled = false
-        textView.delegate = context.coordinator
+//        textView.isEditable = false
+//        textView.isSelectable = true
+//        textView.isScrollEnabled = false
+//        textView.delegate = context.coordinator
         
-        textView.textContainer.lineBreakMode = .byCharWrapping
-        textView.textContainer.lineFragmentPadding = 0
+//        textView.textContainer.lineBreakMode = .byCharWrapping
+//        textView.textContainer.lineFragmentPadding = 0
         textView.textContainerInset = .zero
+        textView.numberOfLines = 0
         
         return textView
     }
     
-    func updateUIView(_ uiView: UITextView, context: Context) {
+    func updateUIView(_ uiView: YYLabel, context: Context) {
         uiView.attributedText = text
     }
     
-    func transfromAttributedText() -> NSAttributedString {
-        let newAttributedString = AttributedString() + attributedString
-        let nsAttributedString = NSMutableAttributedString(newAttributedString)
+    func transfromAttributedText(_ attributedString: AttributedString) -> NSAttributedString {
+        let nsAttributedString = NSMutableAttributedString(attributedString)
         
         // 遍历所有runs并应用字体
         attributedString.runs.enumerated().forEach { index, run in
@@ -190,7 +239,7 @@ struct SelectableText: UIViewRepresentable {
     }
     
     @available(iOS 16.0, *)
-    func sizeThatFits(_ proposal: ProposedViewSize, uiView: UITextView, context: Context) -> CGSize? {
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: YYLabel, context: Context) -> CGSize? {
         guard let width = proposal.width else { return nil }
         let dimensions = text.boundingRect(
             with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
@@ -201,21 +250,21 @@ struct SelectableText: UIViewRepresentable {
 }
 
 extension SelectableText {
-    final class Coordinator: NSObject, UITextViewDelegate {
+    final class Coordinator: NSObject, YYTextViewDelegate {
         private var textView: SelectableText
         init(_ textView: SelectableText) {
             self.textView = textView
             super.init()
         }
-        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        func textView(_ textView: YYTextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
             return true
         }
-        func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        func textView(_ textView: YYTextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
             self.textView.openURL(URL)
             return false
         }
-        func textViewDidChange(_ textView: UITextView) {
-            self.textView.text = textView.attributedText
+        func textViewDidChange(_ textView: YYTextView) {
+            self.textView.text = textView.attributedText ?? .init(string: "")
         }
     }
 }
---
+++ Sources/YYText/include/YYText.h
@@ -0,0 +1,29 @@
+//
+//  YYText.h
+//  YYText <https://github.com/ibireme/YYText>
+//
+//  Created by ibireme on 15/2/25.
+//  Copyright (c) 2015 ibireme.
+//
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YYLabel.h"
+#import "YYTextView.h"
+#import "YYTextAttribute.h"
+#import "YYTextArchiver.h"
+#import "YYTextParser.h"
+#import "YYTextRunDelegate.h"
+#import "YYTextRubyAnnotation.h"
+#import "YYTextLayout.h"
+#import "YYTextLine.h"
+#import "YYTextInput.h"
+#import "YYTextDebugOption.h"
+#import "YYTextKeyboardManager.h"
+#import "YYTextUtilities.h"
+#import "NSAttributedString+YYText.h"
+#import "NSParagraphStyle+YYText.h"
+#import "UIPasteboard+YYText.h"
