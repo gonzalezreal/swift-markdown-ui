5a6cf387e9c41993b08ddc79bb438a1d024fb5eb
Jason van den Berg <jayvdb1@gmail.com>
2025-12-18 14:19:49 +0200

    diff example file

---
+++ Examples/Demo/Demo/DiffView.swift
@@ -0,0 +1,18 @@
+//
+//  DiffView.swift
+//  Demo
+//
+//  Created by Jason van den Berg on 2025/12/18.
+//
+
+import SwiftUI
+
+struct DiffView: View {
+    var body: some View {
+			Text("TODO")
+    }
+}
+
+#Preview {
+    DiffView()
+}
